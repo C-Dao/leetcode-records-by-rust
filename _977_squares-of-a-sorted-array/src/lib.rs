006288587124466aa4d3c08ed20ba36626c11468
BenMix <wubendao@gmail.com>
2022-07-14 01:18:10 +0800

    feat: 977

--- _977_squares-of-a-sorted-array/src/lib.rs
+++ _977_squares-of-a-sorted-array/src/lib.rs
@@ -8,6 +8,33 @@ struct Solution {}
 
 // @lc code=start
 impl Solution {
-    pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {}
+    pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
+        let (mut start, mut end, mut point) = (0, nums.len() - 1, nums.len() - 1);
+        let mut result = vec![0; nums.len()];
+        loop {
+            if nums[start] + nums[end] > 0 {
+                result[point] = nums[end] * nums[end];
+                if point == 0 {
+                    break;
+                }
+                point -= 1;
+                if end == 0 {
+                    break;
+                }
+                end -= 1;
+            } else {
+                result[point] = nums[start] * nums[start];
+                if point == 0 {
+                    break;
+                }
+                point -= 1;
+                if start == nums.len() - 1 {
+                    break;
+                }
+                start += 1;
+            }
+        }
+        result
+    }
 }
 // @lc code=end
