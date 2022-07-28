a8f4f06da3fd1994dce65ab740720ae0e567b730
BenMix <wubendao@gmail.com>
2022-07-29 01:10:46 +0800

    feat: 202

---
+++ _math/_202_happy-number/src/main.rs
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=202 lang=rust
+ *
+ * [202] Happy Number
+ */
+
+struct Solution {}
+
+// @lc code=start
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {}
+}
+// @lc code=end
+
+fn main() {
+    println!("Hello, world!");
+}
