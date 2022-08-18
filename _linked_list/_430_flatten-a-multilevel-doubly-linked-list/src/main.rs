9848ee5b0ba4922802080fb1a211a6faeae7111e
BenMix <wubendao@gmail.com>
2022-08-19 02:37:07 +0800

    feat: 61

--- _linked_list/_430_flatten-a-multilevel-doubly-linked-list/src/main.rs
+++ _linked_list/_430_flatten-a-multilevel-doubly-linked-list/src/main.rs
@@ -1,3 +1,86 @@
+/*
+ * @lc app=leetcode id=430 lang=rust
+ *
+ * [430] Flatten a multilevel doubly linked list
+ */
+
+use std::{cell::RefCell, rc::Rc, rc::Weak};
+
+ struct Solution {}
+// @lc code=start
+#[derive(Debug, Clone)]
+struct DoublyNode {
+    val: i32,
+    next: Option<Rc<RefCell<DoublyNode>>>,
+    prev: Option<Weak<RefCell<DoublyNode>>>,
+    child: Option<Rc<RefCell<DoublyNode>>>,
+}
+
+
+impl DoublyNode {
+    fn new(val: i32) -> Self {
+        DoublyNode {
+            val,
+            next: None,
+            prev: None,
+            child: None,
+        }
+    }
+}
+
+impl Solution {
+ pub fn flatten(head: Option<Rc<RefCell<DoublyNode>>>) -> Option<Rc<RefCell<DoublyNode>>> {
+    Solution::back_track(head.clone());   
+    return head;
+ }
+
+fn back_track(head: Option<Rc<RefCell<DoublyNode>>>) -> (Option<Rc<RefCell<DoublyNode>>>,Option<Rc<RefCell<DoublyNode>>>) {
+    let mut dummy_cur = Rc::new(RefCell::new(DoublyNode::new(-1)));
+    dummy_cur.borrow_mut().next = head.clone();
+
+    let mut tail = head.clone();
+
+    while dummy_cur.borrow_mut().next.is_some(){
+        let next_node = dummy_cur.borrow_mut().next.take();
+        let child_node = dummy_cur.borrow_mut().child.take();
+        
+        if child_node.is_some(){  
+            let (child_head, child_tail) = Solution::back_track(child_node);
+
+            let child_head_cur = match child_head {
+                Some(ref node) => Rc::clone(node),
+                None => return (None, None),
+            };
+
+            let child_tail_cur = match child_tail {
+                Some(ref node) => Rc::clone(node),
+                None => return (None, None),
+            };
+
+            child_head_cur.borrow_mut().prev = Some(Rc::downgrade(&dummy_cur));
+            child_tail_cur.borrow_mut().next = next_node;
+            
+            dummy_cur.borrow_mut().next = child_head;
+            dummy_cur.borrow_mut().child = None;
+            dummy_cur = child_tail_cur.borrow().next.clone().unwrap();
+        
+        }else{
+            dummy_cur = next_node.clone().unwrap();
+        }
+
+        tail = Some(dummy_cur.clone());
+    }
+
+     (head, tail)
+ }
+}
+
+// @lc code=end
+
+
 fn main() {
+
+
+
     println!("Hello, world!");
 }
---
+++ _linked_list/_61_rotate-list/src/main.rs
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=61 lang=rust
+ *
+ * [61] Rotate List
+ */
+
+
+ struct Solution{}
+
+// Definition for singly-linked list.
+ #[derive(PartialEq, Eq, Clone, Debug)]
+ pub struct ListNode {
+   pub val: i32,
+   pub next: Option<Box<ListNode>>
+ }
+ 
+ impl ListNode {
+   #[inline]
+   fn new(val: i32) -> Self {
+     ListNode {
+       next: None,
+       val
+     }
+   }
+ }
+
+// @lc code=start
+
+impl Solution {
+    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        let mut len = 0;
+        let mut temp_head = head.as_ref();
+        while let Some(node) = temp_head {
+            len += 1;
+            temp_head = node.next.as_ref();
+        }
+
+        if len == 0 {
+            return None;
+        }
+ 
+        let k = k.rem_euclid(len as i32);
+
+        if k == 0 {
+            return head;
+        }
+
+
+        let mut old_head = head.take().unwrap();
+
+
+        let mut node = old_head.as_mut();
+       
+        for _ in 0..len-k-1 {
+            node = node.next.as_mut().unwrap();
+        }
+
+        let mut new_head = node.next.take().unwrap();
+        let mut new_tail = new_head.as_mut();
+
+        for _ in 0..k-1 {
+            new_tail = new_tail.next.as_mut().unwrap();
+        }
+
+        new_tail.next = Some(old_head);
+        Some(new_head) 
+    }
+}
+// @lc code=end
+
+
+fn main() {
+    let mut a = Box::new(ListNode::new(1));
+    let mut b = Box::new(ListNode::new(2));
+    let mut c = Box::new(ListNode::new(3));
+    let mut d = Box::new(ListNode::new(4));
+    let e = Box::new(ListNode::new(5));
+
+    d.next = Some(e);
+    c.next = Some(d);
+    b.next = Some(c);
+    a.next = Some(b);
+
+    println!("{:?}", Solution::rotate_right(Some(a),2));
+}
