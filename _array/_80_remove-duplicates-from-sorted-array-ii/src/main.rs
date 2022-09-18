a7062170627e0a47af46b9008e5dea74bf6f4c9a
BenMix <wubendao@gmail.com>
2022-09-19 01:06:40 +0800

    feat: 80

--- _array/_80_remove-duplicates-from-sorted-array-ii/src/main.rs
+++ _array/_80_remove-duplicates-from-sorted-array-ii/src/main.rs
@@ -85,6 +85,10 @@ struct Solution {}
 // @lc code=start
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.len() < 3 {
+            return nums.len() as i32;
+        };
+
         let mut i = 2;
         for j in 2..nums.len() {
             if nums[j] != nums[i - 2] {
@@ -98,5 +102,8 @@ impl Solution {
 // @lc code=end
 
 fn main() {
-    println!("{}", Solution::remove_duplicates(&mut vec![0,0,1,1,1,1,2,3,3]));
+    println!(
+        "{}",
+        Solution::remove_duplicates(&mut vec![0, 0, 1, 1, 1, 1, 2, 3, 3])
+    );
 }
