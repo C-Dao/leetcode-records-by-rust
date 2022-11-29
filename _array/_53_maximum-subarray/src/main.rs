920ab7490d70c3ed8cc20df70804236805344bcd
BenMix <wubendao@gmail.com>
2022-11-30 06:20:49 +0800

    feat: 53,72

---
+++ _array/_53_maximum-subarray/src/main.rs
@@ -0,0 +1,128 @@
+/*
+ * @lc app=leetcode id=53 lang=rust
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Medium (49.69%)
+ * Likes:    26226
+ * Dislikes: 1180
+ * Total Accepted:    2.8M
+ * Total Submissions: 5.7M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the subarray which has the largest sum and
+ * return its sum.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1]
+ * Output: 1
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [5,4,-1,7,8]
+ * Output: 23
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ *
+ * Follow up: If you have figured out the O(n) solution, try coding another
+ * solution using the divide and conquer approach, which is more subtle.
+ *
+ */
+
+struct RangeState {
+    l_max_sum: i32,
+    r_max_sum: i32,
+    max_sum: i32,
+    range_sum: i32,
+}
+
+struct Solution {}
+
+// @lc code=start
+impl Solution {
+    pub fn max_sub_array(mut nums: Vec<i32>) -> i32 {
+        let mut max = nums[0];
+        for i in 1..nums.len() {
+            if nums[i] + nums[i - 1] > nums[i] {
+                nums[i] += nums[i - 1];
+            };
+
+            if nums[i] > max {
+                max = nums[i];
+            }
+        }
+        max
+    }
+
+    pub fn max_sub_array_2(nums: Vec<i32>) -> i32 {
+        let len = nums.len();
+        Self::get(&nums, 0, len - 1).max_sum
+    }
+
+    fn push_up(l_range: RangeState, r_range: RangeState) -> RangeState {
+        RangeState {
+            l_max_sum: i32::max(l_range.range_sum + r_range.l_max_sum, l_range.l_max_sum),
+            r_max_sum: i32::max(r_range.range_sum + l_range.r_max_sum, r_range.r_max_sum),
+            max_sum: i32::max(
+                i32::max(l_range.max_sum, r_range.max_sum),
+                l_range.r_max_sum + r_range.l_max_sum,
+            ),
+            range_sum: l_range.range_sum + r_range.range_sum,
+        }
+    }
+
+    fn get(nums: &Vec<i32>, l: usize, r: usize) -> RangeState {
+        if l == r {
+            return RangeState {
+                l_max_sum: nums[l],
+                r_max_sum: nums[l],
+                max_sum: nums[l],
+                range_sum: nums[l],
+            };
+        }
+        let m = (l + r) >> 1;
+        let l_range_state_sub = Self::get(nums, l, m);
+        let r_range_state_sub = Self::get(nums, m + 1, r);
+        Self::push_up(l_range_state_sub, r_range_state_sub)
+    }
+}
+// @lc code=end
+
+fn main() {
+    assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+    assert_eq!(Solution::max_sub_array(vec![1]), 1);
+    assert_eq!(
+        Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+        6
+    );
+    assert_eq!(Solution::max_sub_array_2(vec![5, 4, -1, 7, 8]), 23);
+    assert_eq!(Solution::max_sub_array_2(vec![1]), 1);
+    assert_eq!(
+        Solution::max_sub_array_2(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+        6
+    );
+}
--- _dynamic_programming/_72_edit-distance/src/main.rs
+++ _dynamic_programming/_72_edit-distance/src/main.rs
@@ -15,55 +15,89 @@
  *
  * Given two strings word1 and word2, return the minimum number of operations
  * required to convert word1 to word2.
- * 
+ *
  * You have the following three operations permitted on a word:
- * 
- * 
+ *
+ *
  * Insert a character
  * Delete a character
  * Replace a character
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: word1 = "horse", word2 = "ros"
  * Output: 3
- * Explanation: 
+ * Explanation:
  * horse -> rorse (replace 'h' with 'r')
  * rorse -> rose (remove 'r')
  * rose -> ros (remove 'e')
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: word1 = "intention", word2 = "execution"
  * Output: 5
- * Explanation: 
+ * Explanation:
  * intention -> inention (remove 't')
  * inention -> enention (replace 'i' with 'e')
  * enention -> exention (replace 'n' with 'x')
  * exention -> exection (replace 'n' with 'c')
  * exection -> execution (insert 'u')
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 0 <= word1.length, word2.length <= 500
  * word1 and word2 consist of lowercase English letters.
- * 
- * 
+ *
+ *
  */
+
+struct Solution {}
 
 // @lc code=start
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
-        
+        let col_len = word1.len();
+        let row_len = word2.len();
+        let word1_bytes = word1.as_bytes();
+        let word2_bytes = word2.as_bytes();
+        let mut dp = vec![vec![0; col_len + 1]; row_len + 1];
+
+        for i in 0..=row_len {
+            dp[i][0] = i;
+        }
+
+        for i in 0..=col_len {
+            dp[0][i] = i;
+        }
+
+        for row in 1..=row_len {
+            for col in 1..=col_len {
+                if word1_bytes[col - 1] == word2_bytes[row - 1] {
+                    dp[row][col] = dp[row - 1][col - 1];
+                } else {
+                    dp[row][col] = usize::min(
+                        dp[row - 1][col - 1],
+                        usize::min(dp[row][col - 1], dp[row - 1][col]),
+                    ) + 1;
+                }
+            }
+        }
+        dp[row_len][col_len] as i32
     }
 }
 // @lc code=end
 
+fn main() {
+    assert_eq!(
+        Solution::min_distance(String::from("horse"), String::from("ros")),
+        3
+    );
+}
