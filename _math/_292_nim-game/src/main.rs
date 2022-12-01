d4069eeac56fe35e7f35a2fb7fc36c4a9f4f1653
BenMix <wubendao@gmail.com>
2022-12-01 22:54:22 +0800

    feat: 292

--- _math/_292_nim-game/src/main.rs
+++ _math/_292_nim-game/src/main.rs
@@ -14,23 +14,23 @@
  * Testcase Example:  '4'
  *
  * You are playing the following Nim Game with your friend:
- * 
- * 
+ *
+ *
  * Initially, there is a heap of stones on the table.
  * You and your friend will alternate taking turns, and you go first.
  * On each turn, the person whose turn it is will remove 1 to 3 stones from the
  * heap.
  * The one who removes the last stone is the winner.
- * 
- * 
+ *
+ *
  * Given n, the number of stones in the heap, return true if you can win the
  * game assuming both you and your friend play optimally, otherwise return
  * false.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: n = 4
  * Output: false
  * Explanation: These are the possible outcomes:
@@ -41,36 +41,41 @@
  * 3. You remove 3 stones. Your friend removes the last stone. Your friend
  * wins.
  * In all outcomes, your friend wins.
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: n = 1
  * Output: true
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: n = 2
  * Output: true
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= n <= 2^31 - 1
- * 
- * 
+ *
+ *
  */
+
+struct Solution {}
 
 // @lc code=start
 impl Solution {
     pub fn can_win_nim(n: i32) -> bool {
-        
+        n % 4 != 0
     }
 }
 // @lc code=end
 
+fn main() {
+    assert_eq!(Solution::can_win_nim(4), false);
+}
